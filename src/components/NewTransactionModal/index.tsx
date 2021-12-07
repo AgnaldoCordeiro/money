6a06bbd92b1d75baa4700663db965274ac068a44
AgnaldoCordeiro <agske13@gmail.com>
2021-12-07 15:28:15 -0300

    adicionando função para somar totais e  hook e finalizando projeto

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,10 @@
 import Modal from 'react-modal'
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
+import { FormEvent, useState } from 'react'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { FormEvent, useState } from 'react'
-import {api} from '../../services/api'
+import { useTransactions } from '../../hooks/useTransactions'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -12,25 +12,28 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const { createTransaction } = useTransactions();
   const [type, setType] = useState('deposit'),
     [title, setTitle] = useState(''),
-    [value, setValue] = useState(0),
+    [amount, setAmount] = useState(0),
     [category, setCategory] = useState('');  
   
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
+     await createTransaction({
       title,
-      value,
+      amount,
       category,
       type
-    };
+    })
 
-    api.post('transactions', data)
-  }
-
-  
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
+  } 
   
   return (
       <Modal
@@ -54,8 +57,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
